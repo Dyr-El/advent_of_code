be5a60b858f140892ab063f4063d1e6903bf1455
SSSCKR <christer.lennell@scania.com>
2025-11-21 07:38:26 +0100

    NoChage

---
+++ 2024/lennellen-java/src/com/lennell/Day1.java
@@ -0,0 +1,54 @@
+package com.lennell;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day1 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Day 1");
+
+        List<Integer> leftNumbers = new ArrayList<>();
+        List<Integer> rightNumbers = new ArrayList<>();
+
+
+        List<String> lines = Files.readAllLines(Path.of("src/com/lennell/test1.txt"));
+        for (String line : lines) {
+            String[] parts = line.trim().split("\\s+");
+            if (parts.length == 2) {
+                leftNumbers.add(Integer.parseInt(parts[0]));
+                rightNumbers.add(Integer.parseInt(parts[1]));
+            }
+        }
+
+
+        List<Integer> sortedLeft = leftNumbers.stream().sorted().toList();
+        List<Integer> sortedRight = rightNumbers.stream().sorted().toList();
+        if (sortedLeft.size() != sortedRight.size()) {
+            System.out.println("Lists are of different sizes");
+            return;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < sortedLeft.size(); i++) {
+            sum += Math.abs(sortedLeft.get(i) - sortedRight.get(i));
+
+        }
+        System.out.println("Sum of paired elements: " + sum);
+
+        final int sum2 = 0;
+
+        for (Integer l: leftNumbers){
+            System.out.print(l + " : ");
+            System.out.println(rightNumbers.stream().filter( r -> r==l).count());
+        }
+
+
+        System.out.println("Sum part 2 of paired elements: " + sum);
+
+
+    }
+}
