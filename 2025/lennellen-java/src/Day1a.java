5f24fd3d9383e391c1cd18d91a72af3ebb2027db
SSSCKR <christer.lennell@scania.com>
2025-12-02 16:59:38 +0100

    Day1a

--- 2025/lennellen-java/src/Day1a.java
+++ 2025/lennellen-java/src/Day1a.java
@@ -3,10 +3,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
-public class Day1 {
+public class Day1a {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Day 1");
