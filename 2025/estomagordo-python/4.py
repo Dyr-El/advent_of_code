1ef133e35b61231f491450967d49f90801b3bb89
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2025-12-04 08:59:34 +0100

    done through 4

--- 2025/estomagordo-python/4.py
+++ 2025/estomagordo-python/4.py
@@ -8,51 +8,54 @@ from math import ceil, comb, factorial, gcd, isclose, lcm
 from algo import a_star, custsort, merge_ranges, sssp
 from constants import DIRECTIONS, EPSILON, HUGE, UNHUGE
 from helpers import adjacent, between, chunks, chunks_with_overlap, columns, digits, dimensions, distance, distance_sq, eight_neighs, eight_neighs_bounded, find_in_grid, forward_rays_with_diagonals, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded, overlap, positives, rays, rays_from_inside, solve_system, words
-
+ 
 
 def parse(lines):
-    return [ints(' '.join(list(line.rstrip()))) for line in lines]
+    return [list(line.rstrip()) for line in lines]
 
-
-def make_num(digits):
-    return int(''.join(str(digit) for digit in digits))
-
-
-def extract_max(bank, needed):
-    n = len(bank)
-
-    @cache
-    def solve(taken, pos):
-        if taken == needed:
-            return 0
-        
-        if pos == n:
-            return UNHUGE
-        
-        skip = solve(taken, pos+1)
-        take = 10**(needed-taken-1) * bank[pos] + solve(taken+1, pos+1)
-
-        return max(skip, take)
-    
-    return solve(0, 0)
-    
 
 def solve_a(lines):
-    banks = parse(lines)
+    grid = parse(lines)
 
-    return sum(extract_max(bank, 2) for bank in banks)
+    h = len(grid)
+    w = len(grid[0])
+    count = 0
+
+    for y, x in product(range(h), range(w)):
+        if grid[y][x] == '@' and sum(grid[i][j] == '@' for i,j in eight_neighs_bounded(y, x, 0, h-1, 0, w-1)) < 4:
+            count += 1
+
+    return count
 
 
 def solve_b(lines):
-    banks = parse(lines)
+    grid = parse(lines)
 
-    return sum(extract_max(bank, 12) for bank in banks)
+    h = len(grid)
+    w = len(grid[0])
+    count = 0
+
+    removing = True
+    while removing:
+        removing = False
+        to_remove = []
+
+        for y, x in product(range(h), range(w)):
+            if grid[y][x] == '@' and sum(grid[i][j] == '@' for i,j in eight_neighs_bounded(y, x, 0, h-1, 0, w-1)) < 4:
+                to_remove.append((y, x))
+                count += 1
+
+        for y, x in to_remove:
+            removing = True
+            grid[y][x] = '.'
+
+    return count
 
 
 def main():
     lines = []
 
-    with open('3.txt') as f:
+    with open('4.txt') as f:
         for line in f.readlines():
             lines.append(line)
             
